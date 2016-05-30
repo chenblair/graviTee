175421291963765261e4c18ea3ada7e069edfe6a
unbrace3 <unbrace3@gmail.com>
2016-05-29 22:53:27 -0700

    Gravity Well
    extremely buggy but works somewhat,
    object is drawn to anchor

---
+++ gravityWell.py
@@ -0,0 +1,81 @@
+# Python Cocos2d Game Development
+# Part 1: Getting Started
+
+# Tutorial: http://jpwright.net/writing/python-cocos2d-game-1/
+# Github: http://github.com/jpwright/cocos2d-python-tutorials
+
+# Jason Wright (jpwright0@gmail.com)
+
+
+# Imports
+
+from random import randint
+
+import pyglet
+from pyglet.window import key
+
+import cocos
+from cocos import actions, layer, sprite, scene
+from cocos.director import director
+
+# Player class
+
+class Me(actions.Move):
+  
+  # step() is called every frame.
+  # dt is the number of seconds elapsed since the last call.
+  def step(self, dt):
+    
+    super(Me, self).step(dt) # Run step function on the parent class.
+    myX = me.position[0]
+    myY = me.position[1]
+    aX = anchor.position[0] 
+    aY = anchor.position[1]
+    
+    graviX = 10000/((-myX+aX)*abs(myX-aX))
+    graviY = 10000/((-myY+aY)*abs(myY-aY))
+    
+    # Determine velocity based on keyboard inputs.
+    velocity_x = 200+graviX
+    velocity_y = 500 * (keyboard[key.UP] - keyboard[key.DOWN])+graviY
+    
+    # Set the object's velocity.
+    self.target.velocity = (velocity_x, velocity_y)
+    
+# Main class
+
+def main():
+  global keyboard,myX,myY,aX,aY,me,anchor # Declare this as global so it can be accessed within class methods.
+  
+  # Initialize the window.
+  director.init(width=500, height=300, do_not_scale=True, resizable=True)
+  
+  # Create a layer and add a sprite to it.
+  player_layer = layer.Layer()
+  me = sprite.Sprite('ball.jpg')
+  player_layer.add(me)
+  
+  anchor = sprite.Sprite('ball.jpg')
+  player_layer.add(anchor)
+  
+  # Set initial position and velocity.
+  me.position = (0, randint(0,200))
+  me.velocity = (0, 0)
+  
+  anchor.position = (100,100)
+  
+  # Set the sprite's movement class.
+  me.do(Me())
+
+  # Create a scene and set its initial layer.
+  main_scene = scene.Scene(player_layer)
+
+  # Attach a KeyStateHandler to the keyboard object.
+  keyboard = key.KeyStateHandler()
+  director.window.push_handlers(keyboard)
+
+  # Play the scene in the window.
+  director.run(main_scene)
+
+if __name__ == '__main__':
+    main()
