c6e9760302dabdae862871fb204967e28d2cb44f
unbrace3 <unbrace3@gmail.com>
2016-05-30 16:27:15 -0700

    gravity with multiple anchors
    used python lists

--- gravityWell.py
+++ gravityWell.py
@@ -23,7 +23,7 @@ from math import sqrt
 global prevVelocity_y,prevVelocity_x;
 
 class Me(actions.Move):
-    prevVelocity_x = -100
+    prevVelocity_x = 10
     prevVelocity_y = 0
   
   # step() is called every frame.
@@ -31,18 +31,21 @@ class Me(actions.Move):
     def step(self, dt):
     
         super(Me, self).step(dt) # Run step function on the parent class.
+        G=100.0
         myX = me.position[0]
         myY = me.position[1]
-        aX = anchor.position[0] 
-        aY = anchor.position[1]
-        dist=sqrt((myX-aX)**2+(myY-aY)**2)
-        G=100.0
-        graviX = G*-(myX-aX)/(dist**2)
-        graviY = G*-(myY-aY)/(dist**2)
+        velocity_x = self.prevVelocity_x
+        velocity_y = self.prevVelocity_y+500 * (keyboard[key.UP] - keyboard[key.DOWN])
+        for i in range(numAnchors) :
+            aX = anchors[i].position[0] 
+            aY = anchors[i].position[1]
+            dist=sqrt((myX-aX)**2+(myY-aY)**2)
+            graviX = G*-(myX-aX)/(dist**2)
+            graviY = G*-(myY-aY)/(dist**2)
     
-        # Determine velocity based on keyboard inputs.
-        velocity_x = self.prevVelocity_x+graviX
-        velocity_y = self.prevVelocity_y+500 * (keyboard[key.UP] - keyboard[key.DOWN])+graviY
+            # Determine velocity based on keyboard inputs.
+            velocity_x += graviX
+            velocity_y += graviY
     
         self.prevVelocity_x = velocity_x
         self.prevVelocity_y = velocity_y
@@ -54,7 +57,7 @@ class Me(actions.Move):
 # Main class
 
 def main():
-  global keyboard,myX,myY,aX,aY,me,anchor # Declare this as global so it can be accessed within class methods.
+  global keyboard,myX,myY,aX,aY,me,anchors,numAnchors # Declare this as global so it can be accessed within class methods.
   
   # Initialize the window.
   director.init(width=500, height=300, do_not_scale=True, resizable=True)
@@ -62,19 +65,21 @@ def main():
   prevVelocity_x = 200
   prevVelocity_y = 0
   
+  numAnchors=3
   # Create a layer and add a sprite to it.
   player_layer = layer.Layer()
   me = sprite.Sprite('ball.jpg')
   player_layer.add(me)
+  anchors = []
+  for i in range(numAnchors) :
+      anchors.append(sprite.Sprite('ball.jpg'))
+      player_layer.add(anchors[i])
+      anchors[i].position = (randint(0,600),randint(0,600))
   
-  anchor = sprite.Sprite('ball.jpg')
-  player_layer.add(anchor)
   
   # Set initial position and velocity.
   me.position = (200, 300)
   me.velocity = (0, 0)
-  
-  anchor.position = (200,200)
   
   # Set the sprite's movement class.
   me.do(Me())
