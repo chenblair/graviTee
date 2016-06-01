3f74e77c9f8686dc4efdd76f4ef82f421aa9c21b
unbrace3 <unbrace3@gmail.com>
2016-05-31 22:52:24 -0700

    Attempt at clickevents
    Mouse class works at detecting but Me doesn’t. I think it has something
    to do with cocos.layer.layer, but I can’t figure it out. director.run
    runs the two scenes.
    
    attempting to get Me to detect mouse clicks

--- gravityWell.py
+++ gravityWell.py
@@ -22,9 +22,14 @@ from math import sqrt
 # Player class
 global prevVelocity_y,prevVelocity_x;
 
-class Me(actions.Move):
+class Me(actions.Move,cocos.layer.Layer):
     prevVelocity_x = 10
     prevVelocity_y = 0
+    
+    is_event_handler = True     #: enable director.window events
+    
+    def __init__(self):
+        super( Me, self ).__init__()
   
   # step() is called every frame.
   # dt is the number of seconds elapsed since the last call.
@@ -53,7 +58,45 @@ class Me(actions.Move):
         # Set the object's velocity.
         self.target.velocity = (velocity_x, velocity_y)
         print(velocity_x,velocity_y)
+    def on_mouse_press (self, x, y, buttons, modifiers):
+        """This function is called when any mouse button is pressed
+        (x, y) are the physical coordinates of the mouse
+        'buttons' is a bitwise or of pyglet.window.mouse constants LEFT, MIDDLE, RIGHT
+        'modifiers' is a bitwise or of pyglet.window.key modifier constants
+        (values like 'SHIFT', 'OPTION', 'ALT')
+        """
+        anchors.append(sprite.Sprite('ball.jpg'))
+        player_layer.add(anchors[i])
+        anchors[i].position = (x,y)
+        numAnchors += 1
     
+class Mouse(cocos.layer.Layer):
+
+    is_event_handler = True     #: enable director.window events
+
+    def __init__(self):
+        super( Mouse, self ).__init__()
+
+        self.posx = 100
+        self.posy = 240
+        self.text = cocos.text.Label('No mouse events yet', font_size=18, x=self.posx, y=self.posy )
+        self.add( self.text )
+
+    def update_text (self, x, y):
+        text = 'Mouse @ %d,%d' % (x, y)
+        self.text.element.text = text
+        self.text.element.x = self.posx
+        self.text.element.y = self.posy
+    def on_mouse_press (self, x, y, buttons, modifiers):
+        """This function is called when any mouse button is pressed
+
+        (x, y) are the physical coordinates of the mouse
+        'buttons' is a bitwise or of pyglet.window.mouse constants LEFT, MIDDLE, RIGHT
+        'modifiers' is a bitwise or of pyglet.window.key modifier constants
+        (values like 'SHIFT', 'OPTION', 'ALT')
+        """
+        self.posx, self.posy = director.get_virtual_coordinates (x, y)
+        self.update_text (x,y)
 # Main class
 
 def main():
@@ -65,7 +108,7 @@ def main():
   prevVelocity_x = 200
   prevVelocity_y = 0
   
-  numAnchors=3
+  numAnchors=10
   # Create a layer and add a sprite to it.
   player_layer = layer.Layer()
   me = sprite.Sprite('ball.jpg')
@@ -90,7 +133,7 @@ def main():
   # Attach a KeyStateHandler to the keyboard object.
   keyboard = key.KeyStateHandler()
   director.window.push_handlers(keyboard)
-
+  director.run( cocos.scene.Scene( Mouse() ) )
   # Play the scene in the window.
   director.run(main_scene)
 
