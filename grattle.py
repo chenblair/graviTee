5499acd10844e5f11c1a6cd696bb1f8cedfb0a79
Merrycatch22 <tarn.nicholas@gmail.com>
2016-06-23 12:26:32 -0700

    graphics improved, out of bounds cross added

--- grattle.py
+++ grattle.py
@@ -50,9 +50,8 @@ class Player1(actions.Move):
     
     global timer1
     timer1+=dt
-    print(timer1)
     if keyboard[key.SPACE] and timer1>=1:
-        anchors.append(sprite.Sprite('ball.jpg'))
+        anchors.append(sprite.Sprite('ball.png'))
         player_layer.add(anchors[-1])
         anchors[-1].position = (player1.position[0],player1.position[1])
         timer1=0
@@ -85,7 +84,7 @@ class Player2(actions.Move):
     global timer2
     timer2+=dt
     if keyboard[key.Z] and timer2>=1:
-        anchors.append(sprite.Sprite('ball.jpg'))
+        anchors.append(sprite.Sprite('ball.png'))
         player_layer.add(anchors[-1])
         anchors[-1].position = (player2.position[0],player2.position[1])
         timer2=0
@@ -128,7 +127,7 @@ class Ball(actions.Move):
         global player1_dead, player2_dead
         if result['dist']<(player1.width+ball.width)/2 and player2_dead == False and player1_dead == False:
             player1.stop()
-            self.text = cocos.text.Label('Player 1 loses', font_size=18, x=player1.position[0], y=player1.position[1] )
+            self.text = cocos.text.Label('Player 1 loses', font_size=18, x=player1._x, y=player1.position[1] )
             player_layer.add( self.text )
             player1_dead=True
         velocity_x += result['graviX']
@@ -148,6 +147,8 @@ class Ball(actions.Move):
         self.target.velocity = (velocity_x, velocity_y)
         logging.info((velocity_x,velocity_y))
         
+        
+        
 class AnchorDrop(cocos.layer.Layer):
     
     # If you want that your layer receives director.window events
@@ -159,18 +160,46 @@ class AnchorDrop(cocos.layer.Layer):
         super( AnchorDrop, self ).__init__()
     """def on_key_press (self, pressed, modifiers):
         if keyboard[key.SPACE]:
-            anchors.append(sprite.Sprite('ball.jpg'))
+            anchors.append(sprite.Sprite('ball.png'))
             player_layer.add(anchors[-1])
             anchors[-1].position = (player1.position[0],player1.position[1])
         if keyboard[key.Z]:
-            anchors.append(sprite.Sprite('ball.jpg'))
+            anchors.append(sprite.Sprite('ball.png'))
             player_layer.add(anchors[-1])
             anchors[-1].position = (player2.position[0],player2.position[1])"""
-    
+
+class Cross(actions.Move):
+    aboveP=False
+    aboveN=False
+    def __init__(self):
+        super( Cross, self ).__init__()
+    def step(self,dt):
+        aboveP=ball.position[1]>300.0/500.0*ball.position[0]
+        aboveN=ball.position[1]>-300.0/500.0*ball.position[0]+300.0
+        if ball.position[0]<0 or ball.position[0]>500 or ball.position[1]<0 or ball.position[1]>300:
+            cross.visible=True
+            if aboveP and aboveN:
+                newX=(300-4-150)*(ball.position[0]-250)/(ball.position[1]-150)+250
+                cross.position=(newX,300-4)
+            elif aboveP and not aboveN:
+                newY=(ball.position[1]-150.0)/(ball.position[0]-250)*(500-4-250)+150
+                cross.position=(0+4,300-newY)
+            elif not aboveP and aboveN:
+                newY=(ball.position[1]-150.0)/(ball.position[0]-250)*(500-4-250)+150
+                cross.position=(500-4,newY)
+            else:
+                newX=(300-4-150)*(ball.position[0]-250)/(ball.position[1]-150)+250
+                cross.position=(500-newX,0+4)
+        else:        
+            cross.visible=False
+        
+        
+            
+       
 # Main class
 
 def main():
-  global keyboard,player1,player2,anchors,player_layer,ball,player1_dead,player2_dead,director # Declare this as global so it can be accessed within class methods.
+  global keyboard,player1,player2,anchors,player_layer,ball,player1_dead,player2_dead,director,cross # Declare this as global so it can be accessed within class methods.
   
   # Initialize the window.
   director.init(width=500, height=300, do_not_scale=True, resizable=True)
@@ -180,14 +209,18 @@ def main():
   player2_dead=False
   # Create a layer and add a sprite to it.
   player_layer = layer.Layer()
-  player1 = sprite.Sprite('ball.jpg')
-  player_layer.add(player1)
+  player1 = sprite.Sprite('ball.png')
+  player_layer.add(player1, 1)
   
   ball = sprite.Sprite('earth-small.png')
-  player_layer.add(ball)
+  player_layer.add(ball, 2)
   
-  player2 = sprite.Sprite('ball.jpg')
-  player_layer.add(player2)
+  player2 = sprite.Sprite('ball.png')
+  player_layer.add(player2, 1)
+  
+  cross=sprite.Sprite('cross.png')
+  player_layer.add(cross, 2)
+  cross.visible=False
   
   # Set initial position and velocity.
   ball.position = (250,150)
@@ -203,9 +236,10 @@ def main():
   ball.do(Ball());
   player1.do(Player1())
   player2.do(Player2())
+  cross.do(Cross())
 
   # Create a scene and set its initial layer.
-  main_scene = scene.Scene(player_layer,AnchorDrop())
+  main_scene = scene.Scene(AnchorDrop(),player_layer)
 
   # Attach a KeyStateHandler to the keyboard object.
   keyboard = key.KeyStateHandler()
